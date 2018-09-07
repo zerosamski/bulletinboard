d42aaee79296d56453b01d569f2c3ce21b1e2b28
Samuel Mulkens <sammulkens@gmail.com>
2018-09-07 11:39:03 +0200

    Dynamic messages page, working post request.

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const { Pool } = require('pg') 
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'bssa_assignments',
+    database: 'bulletinboard',
+    port: '5432',
+  })
+
+let database;
+
+pool.query('select title, body from messages ORDER BY id desc')
+        .then((response) => database = response.rows)
+        .catch(err => console.error('Error executing query', err.stack))
 
 //use bodyparser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,13 +27,31 @@ app.use(express.static('node_modules/bulma/css'));
 //set view enige
 app.set("view engine", "ejs");
 
+//viewing the messages
+app.get('/messages', (req, res) => {
+    pool.query('select title, body from messages ORDER BY id desc')
+        .then((response) => database = response.rows)
+        .catch(err => console.error('Error executing query', err.stack))
+
+  res.render('messages', {database: database})
+})
+
+
 //posting a message
 app.get('/postmessage', (req, res) => {
     res.render('postmessage')
 })
 
+app.post('/postmessage', (req, res) => {
+    title = req.body.title;
+    body = req.body.message;
+    pool.query(`insert into messages (title, body) values ('${title}', '${body}')`)
+        .then((response) => console.log(`${response.command}: ${response.rowCount}`))
+        .catch(err => console.error('Error executing query', err.stack))
+    res.redirect('/messages')
+})
+
+//listening port
 app.listen(3000, () => {
     console.log('Server listening on port 3000!')
 })
-
-
